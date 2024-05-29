d422c957d859f8ee757664219c8ce09ed3787155
Carlos Fernandez Cabrero <56631428+carlosfernandezcabrero@users.noreply.github.com>
2024-05-29 12:55:54 +0200

    chore: Disable devtools in nuxt.config.ts and remove @nuxt/devtools from devDependencies

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: [
     '~/assets/css/global.css',
     'vue-toast-notification/dist/theme-default.css'
